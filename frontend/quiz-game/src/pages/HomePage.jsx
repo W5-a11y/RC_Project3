cd81985b6066d31926506c4ba0d58fe600126bba
Samantha Siew <sammie.siew@gmail.com>
2025-07-18 15:02:21 -0700

    basic frontend pages and functions

---
+++ frontend/quiz-game/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Quiz Game</h1>
+      <a href="/quiz">
+        <button>Start Quiz</button>
+      </a>
+    </div>
+    
+  )
+}
+
+export default HomePage
---
+++ frontend/quiz-game/src/pages/ResultPage.jsx
@@ -0,0 +1,5 @@
+function ResultPage() {
+  return <h2>Your score is ...</h2>
+}
+
+export default ResultPage
